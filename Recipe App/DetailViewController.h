8d509d2da3456cf9aff174206d79a7f361eb44f0
Brock Oberhansley <brockoberhansley@Brocks-MacBook-Pro.local>
2015-09-19 12:20:36 -0600

    middle step 6

---
+++ Recipe App/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Recipe App
+//
+//  Created by Brock Oberhansley on 9/19/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
